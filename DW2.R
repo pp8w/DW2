0258b8e52dbcadea3d6eb15a2e1871f359eba809
pp8w <philip.nmn.park@gmail.com>
2016-07-14 16:01:08 -0400

    Pseudo code with outline

---
+++ DW2.R
@@ -0,0 +1,20 @@
+library(dplyr)
+library(tidyr)
+
+#Load titanic into Rstudio
+
+
+#Embarcation
+#Finding missing values and fill with S, watch for empty
+
+#Age
+#Find mean age of existing values, populate empties with that value
+
+#Lifeboat
+#Fill empty boats column with NA
+
+#Cabin
+#If cabin value, has_cabin_number column = 1
+
+#Output
+#Output as titanic_clean.csv
